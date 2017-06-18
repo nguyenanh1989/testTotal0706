7c3e55a491ef47155da0f86feb630fadd1f28961
nguyenanh1989 <anh.nguyen.van.intern@ntq-solution.com.vn>
2017-06-18 10:40:34 +0700

    Update Exercise4.java

--- src/com/exercise/Exercise4.java
+++ src/com/exercise/Exercise4.java
@@ -5,95 +5,73 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Exercise4 {
-	private final Map<String, Integer> map = new HashMap<String, Integer>();
+	private final Map<Integer, String> map = new HashMap<Integer, String>();
 	String stringNumber = "";
 
 	public Exercise4() {
-		map.put(" One ", 1);
-		map.put(" Two ", 2);
-		map.put(" Three ", 3);
-		map.put(" Four ", 4);
-		map.put("Five", 5);
-		map.put("Six", 6);
-		map.put("Seven", 7);
-		map.put("eight", 8);
-		map.put("Nine",9);
-		map.put("Ten", 10);
-		map.put("Eleven", 11);
-		map.put("twelve", 12);
-		map.put("thirteen", 13);
-		map.put("fifteen", 15);
-		map.put("twenty", 20);
-		map.put("thirty", 30);
-		map.put("fifty", 50);
+		map.put(1, "One");
+		map.put(2, "Two");
+		map.put(3, "Three");
+		map.put(4, "Four");
+		map.put(5, "Five");
+		map.put(6, "Six");
+		map.put(7, "Seven");
+		map.put(8, "eight");
+		map.put(9, "Nine");
+		map.put(10, "Ten");
+		map.put(11, "Eleven");
+		map.put(12, "twelve");
+		map.put(13, "thirteen");
+		map.put(15, "fifteen");
+		map.put(20, "twenty");
+		map.put(30, "thirty");
+		map.put(50, "fifty");
 
 	}
 
 	public String getNumber(int big) {
-		
-		if (big  > 1000000000){
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/1000000000 == map1.getValue()){
-					stringNumber += map1.getKey()+" bilion ";
-					break;
-				}
+
+		if (big > 1000000000) {
+			if (map.containsKey(big / 1000000000)) {
+				stringNumber += map.get(big/1000000000) + " bilion ";
 			}
 			big = big % 1000000000;
-			getNumber(big);
-		} if (big  > 1000000){
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/1000000 == map1.getValue()){
-					stringNumber += map1.getKey()+" milion ";
-					break;
-				}
-			}
+		}
+		if (big > 1000000) {
+			stringNumber += resolverNumber(big/1000000) + " milion ";
 			big = big % 1000000;
-			getNumber(big);
-			
-		} if (big > 1000){
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/1000 == map1.getValue()){
-					stringNumber += map1.getKey()+" thousand ";
-					break;
-				}
-			}
-			big = big % 1000;
-			getNumber(big);
-			
-		} if (big > 100 ){
-			
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/100 == map1.getValue()){
-					stringNumber += map1.getKey()+" hundred ";
-					break;
-				}
-			}
-			big = big % 100;
-			getNumber(big);
-			
-			
-		} if (big > 10){
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/10 == map1.getValue()){
-					stringNumber += map1.getKey()+"ty ";
-					break;
-				}
-			}
-			big = big % 10;
-			getNumber(big);
-			
-		} else{
-			for (Map.Entry<String,Integer> map1 : map.entrySet()){
-				if(big/10 == map1.getValue()){
-					stringNumber += map1.getKey();
-					break;
-				}
-			}
-			big = big % 10;
-			getNumber(big);
-			
+		}
+		if(big > 1000){			
+				stringNumber += resolverNumber(big/1000) + " thounsend ";
+				big = big % 1000;			
+		}
+		if(big > 0){
+			stringNumber += resolverNumber(big);
 		}
 		System.out.println(stringNumber);
+		return stringNumber;
+	}
+
+	public String resolverNumber(int number) {
+		System.out.println(number);
+		String stringNumber = "";
+		if(number > 100 ){
+			stringNumber += map.get(number/100)+" hundred ";
+			number = number % 100;
+		}
+		if(number > 10){
+			if (map.containsKey(number)){
+				return stringNumber += map.get(number);
+			}else{			
+				stringNumber += map.get(number/10)+"ty ";
+				number = number % 10;
+			}
+		
+		}
+		if(number > 0){
+			stringNumber += map.get(number)+ " ";
+		}
+		
 		return stringNumber;
 	}
 
