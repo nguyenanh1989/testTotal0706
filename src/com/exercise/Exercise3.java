9098b4089b128c78fd12b1e10efa1277c4109838
nguyenanh1989 <anh.nguyen.van.intern@ntq-solution.com.vn>
2017-06-18 11:56:34 +0700

    Update Exercise3.java

--- src/com/exercise/Exercise3.java
+++ src/com/exercise/Exercise3.java
@@ -2,35 +2,51 @@ package com.exercise;
 
 public class Exercise3 {
 	private int area = 0;
-	public int maximalSquare(int[][] matrix) {
-		
-		for (int i = 0; i < matrix.length; i++) {
+	int areaSquare = 1;
+	int endX, endY;
 
-			for (int j = 0; j < matrix[0].length; j++) {
+	public int maximalSquare(int[][] matrix) {
+
+		for (int i = 0; i < matrix.length - 1; i++) {
+
+			for (int j = 0; j < matrix[0].length - 1; j++) {
 				if (matrix[i][j] == 1) {
-					calculateArea(i, j, matrix);
+					endX = i + 1;
+					endY = j +1;
+					int numberArea = calculateArea(i, j, matrix);
+					if (area < numberArea) {
+						area = numberArea;
+					}
+					break;
 				}
 
 			}
 
 		}
-		return 0;
+		return area;
 	}
 
-	private void calculateArea(int i1, int j1, int[][] matrix) {
-		for (int i = i1; i < matrix.length-1; i++) {
-
-			for (int j = j1; j < matrix[0].length-1; j++) {
-				if (matrix[i][j] == 1 && matrix[i][j+1] != 0) {
-					
-				}else{
-					
+	// caculate aresquare
+	private int calculateArea(int i1, int j1, int[][] matrix) {
+		int startX = i1, startY = j1, firstCheckNumber = 0;
+		for (int i=i1; i <= endX ; i++) {
+			for (int j=j1; j <= endY ; j++) {
+				if (matrix[i][j] == 1) {
+					firstCheckNumber += 1;
+					System.out.println("first"+firstCheckNumber);
 				}
-
 			}
-
 		}
 
+		if (firstCheckNumber == (endX - startX + 1) * (endX - startX + 1)) {
+			areaSquare = firstCheckNumber;
+			if (endX < matrix.length - 1 && endY < matrix[0].length-1) {
+				endX++;
+				endY++;
+				calculateArea(startX,startY, matrix);
+			}
+		}
+		return areaSquare;
 	}
 
 }
